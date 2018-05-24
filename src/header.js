b345905082da36058c12b8453766e15ed9fdd313
Li Song <iseansay@gmail.com>
2018-05-24 23:54:07 +0800

    Update header.js

--- src/header.js
+++ src/header.js
@@ -50,7 +50,6 @@ const Header = () => {
         <Ear direction="right" size={"4em"} />
       </div>
 
-//       <h2>An interactive Guide</h2>
       <h3>
         By <a href="http://notimportant.org/" style={{
           color: "yellow",
